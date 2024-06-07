e1622c41fb709815937b22ef802f94df58aedddd
jspi <j.spiral@gmail.com>
2024-06-07 13:03:20 +0100

    wip V2.1

--- src/app/(pages)/newsletter/Newsletter.tsx
+++ src/app/(pages)/newsletter/Newsletter.tsx
@@ -16,7 +16,7 @@ import {
 // ******************************************************
 // ******************  SUMMARY CARDS ********************
 // ******************************************************
-const SummaryCard: React.FC<SummaryItemInterface> = ({ title, entries }) => (
+const SummaryCard: React.FC<SummaryItemInterface> = ({title, entries}) => (
     <div className="p-4 bg-white shadow-md rounded-md mb-4 w-full">
         <h3 className="text-xl font-semibold mb-2">{title}</h3>
         <ul className="list-disc pl-20 text-gray-700">
@@ -43,7 +43,7 @@ const MetricCard: React.FC<MetricInterface> = ({metric, value, unit, previous_va
         <div className="bg-white text-gray-900 p-4 rounded-lg shadow-md">
             <h3 className="text-sm text-gray-500 mb-1">{metric}</h3>
             <p className="text-2xl font-bold mb-1">{value.toFixed(2)} {unit}</p>
-            <p className={`text-lg ${isPositive ? 'text-green-500' : 'text-red-500'} flex`}>
+            <p className={`text-lg ${isPositive ? 'text-green-500' : 'text-red-500'} flex items-center`}>
                 {isPositive ? <UpArrow/> : <DownArrow/>} {change.toFixed(2)}%
             </p>
             <p className="text-sm mb-1  text-gray-400">from {previous_relative_time}</p>
@@ -67,6 +67,49 @@ const DownArrow: React.FC = () => (
 );
 
 // ******************************************************
+// *****************  PERTINENCE SCORE  *****************
+// ******************************************************
+const PertinenceGauge: React.FC<{ score: number }> = ({ score }) => {
+    // Determine the fill based on the score (0 to 1, where 1 is fully filled)
+    const fillPercentage = (score / 10) * 100;
+
+    // Calculate the color gradient from red to green
+    const red = Math.max(0, 255 - (score * 25.5));
+    const green = Math.min(255, score * 25.5);
+    const color = `rgb(${red},${green},0)`;
+
+    return (
+        <div className="relative flex items-center justify-center w-[25px]">
+            <svg width="24" height="24" viewBox="0 0 36 36" className="circular-chart absolute">
+                <path
+                    className="circle-bg"
+                    d="M18 2.0845
+                        a 15.9155 15.9155 0 0 1 0 31.831
+                        a 15.9155 15.9155 0 0 1 0 -31.831"
+                    fill="none"
+                    stroke="#eee"
+                    strokeWidth="4"
+                />
+                <path
+                    className="circle"
+                    strokeDasharray={`${fillPercentage}, 100`}
+                    d="M18 2.0845
+                        a 15.9155 15.9155 0 0 1 0 31.831
+                        a 15.9155 15.9155 0 0 1 0 -31.831"
+                    fill="none"
+                    stroke={color}
+                    strokeWidth="4"
+                    strokeLinecap="round"
+                />
+            </svg>
+            <div className=" text-gray-700 text-sm font-bold">{score}</div>
+        </div>
+    );
+};
+
+
+
+// ******************************************************
 // ********************  ARTICLE CARDS *******************
 // ******************************************************
 const ArticleCard: React.FC<ArticleInterface> = ({
@@ -81,6 +124,7 @@ const ArticleCard: React.FC<ArticleInterface> = ({
                                                      author,
                                                      sentiment,
                                                      image,
+                                                     url
                                                  }) => (
     <div id={reference_id} className="flex bg-white shadow-md rounded-md mb-2 p-4">
         {image && (
@@ -88,25 +132,40 @@ const ArticleCard: React.FC<ArticleInterface> = ({
                 src={`data:image/jpeg;base64,${image.substring(11)}`}
                 alt="Article"
                 className="w-48 h-48 object-cover rounded mr-4"
-                style={{ width: '200px', height: '200px' }}
+                style={{width: '200px', height: '200px'}}
             />
         )}
         <div className="flex-1">
             <div className="flex justify-between items-center mb-2">
-                <h3 className="text-xl font-semibold">{title}</h3>
-                <div className={`text-2xl ${sentiment === 'positive' ? 'text-green-500' : 'text-red-500'}`}>
-                    {sentiment === 'positive' ? '👍' : '👎'}
+                <div className="flex items-center">
+                    <h3 className="text-xl font-semibold mr-2">{title}</h3>
+                    {url && (
+                        <a href={url} target="_blank" rel="noopener noreferrer"
+                           className="text-blue-400 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor" width="25" height="25">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1}
+                                      d="M10 13a5 5 0 1 0 7-7l-1.5 1.5a3 3 0 1 1-4 4l-1.5 1.5zM14 10a5 5 0 1 0-7 7l1.5-1.5a3 3 0 1 1 4-4l1.5-1.5z"/>
+                            </svg>
+                        </a>
+                    )}
                 </div>
+                <span
+                    className={`px-2 py-0.5 rounded text-sm ${sentiment === 'positive' ? 'bg-green-200 text-green-800' : sentiment === 'negative' ? 'bg-red-200 text-red-800' : 'bg-gray-200 text-gray-800'}`}>
+                    {sentiment === 'positive' ? 'Positive' : sentiment === 'negative' ? 'Negative' : 'Neutral'}
+                </span>
             </div>
-            <p className="text-gray-600 mb-2">{source} ({localization}) - {author}</p>
-            <hr className="mb-4" />
+            <div className="flex justify-between items-center mb-2">
+                <p className="text-gray-600">{source} ({localization}) - {author}</p>
+                <PertinenceGauge score={pertinence_score}/>
+            </div>
+            <hr className="mb-4"/>
             <p className="text-gray-700 mb-4">{summary}</p>
             <ul className="list-disc pl-5 text-gray-700 mb-4">
                 {analysis.map((item, index) => (
                     <li key={index}>{item}</li>
                 ))}
             </ul>
-            <p className="text-gray-700">Pertinence score: {pertinence_score}</p>
         </div>
     </div>
 );
@@ -164,7 +223,6 @@ const Newsletter = () => {
                     <MetricCard key={metric.metric} {...metric} />
                 ))}
             </div>
-            <h2 className="text-2xl font-bold mb-4">Summary</h2>
             <h2 className="text-2xl font-bold mb-4">Summary</h2>
             <div className="summary-section mb-8">
                 {jsonNL.summary.map((summaryItem) => (
