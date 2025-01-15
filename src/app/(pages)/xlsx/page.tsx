8f65a92d832d15804c84c9bd4bf0fa16dec61a1a
Guillaume Draznieks <guillaume.draznieks@blends.fr>
2025-01-15 05:47:46 -0500

    Wrap XLSXPage component in Suspense for improved loading state handling

--- src/app/(pages)/xlsx/page.tsx
+++ src/app/(pages)/xlsx/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import XLSXPage from "./XLSX";
 
 export default function Home() {
   return (
-    <main className="w-full h-full">
-      <XLSXPage/>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="w-full h-full">
+        <XLSXPage/>
+      </main>
+    </Suspense>
   );
 }
