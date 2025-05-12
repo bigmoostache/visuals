d0481d25b7522f778e8ffeb7fa8695378459d0de
cesar <quirinocesar@gmail.com>
2025-05-12 13:57:42 +0100

    Remove unused components and pages related to XLSX and TXT functionalities; delete CheckboxButtons, Boolean, DataStructure, Date, Enumeration, Fields, Integer, Number, and String components; clean up Extraction and main page files for xtrk and xlsx.

--- src/app/(pages)/lucario/page.tsx
+++
@@ -1,9 +0,0 @@
-import LucarioPage from "./Lucario";
-
-export default function Page() {
-  return (
-    <main className="w-full h-full">
-      <LucarioPage />
-    </main>
-  );
-}
--- src/app/(pages)/plan/page.tsx
+++
@@ -1,9 +0,0 @@
-import PlanPage from "./Plan";
-
-export default function Page() {
-  return (
-    <main className="w-full h-full">
-      <PlanPage />
-    </main>
-  );
-}
